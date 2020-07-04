93b96b3828ed83f83c7c25accad780ccd9f2b0e3
silvagpmiguel <silva.gp.miguel@gmail.com>
2020-07-04 17:38:27 +0100

    init web view

---
+++ web/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <MainPage id="app"/>
+</template>
+
+<script>
+import MainPage from './components/MainPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainPage
+  }
+}
+</script>
+
+<style lang="scss">
+$text-color: #2c3e50;
+$bg-color: #F0F8FF;
+
+body, html{
+  width: 100%;
+  height: 100%;
+  background-color: $bg-color;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: $text-color;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ web/src/components/MainPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <b-container fluid>
+    <b-navbar type="dark" variant="dark">
+      <b-navbar-brand href="#">Cantina App</b-navbar-brand>
+      <b-navbar-nav>
+        <b-nav-item href="#">Home</b-nav-item>
+      </b-navbar-nav>
+      <b-navbar-brand href="#/info">
+        <b-icon icon="info-circle"></b-icon>
+      </b-navbar-brand>
+    </b-navbar>
+
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'MainPage',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+
+</style>
